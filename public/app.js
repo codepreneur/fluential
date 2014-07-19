8a9ea01b051cb43c175dcc612f3d647380c46cc1
Nic Yeeles <n.yeeles@gmail.com>
2014-07-19 23:04:31 +0100

    Limits the length of 'about' content and adds an ellipsis

--- public/app.js
+++ public/app.js
@@ -115,8 +115,11 @@ app.controller('TagsCtrl', ['$scope','$modal','$log','$http','$window','$filter'
   });
 
 
+
+
   $scope.influencersData = {};
 
+  // $scope.influencers = ["ERB"]
   $scope.influencers = ["PewDiePie", "holasoygerman", "smosh", "JennaMarbles", "nigahiga",  "RayWilliamJohnson", "ERB", "SkyDoesMinecraft", "TheFineBros", "portadosfundos", "werevertumorro", "vanossgaming", "CaptainSparklez", "TheSyndicateProject", "elrubiusomg", "vsauce", "collegehumor", "officialpsy", "lady16makeup", "freddiew",   "vitalyzdtv", "speedyw03", "ShaneDawsonTV", "RoosterTeeth", "ElektraRecords", "BlueXephos", "TobyGames", "MichellePhan", "Macbarbie07", "EpicMealtime", "enchufetv", "ksiolajidebt", "vegetta777", "RiotGamesInc", "SpinninRec", "Tobuscus"];
 
   // EXCLUDE THESE
@@ -133,6 +136,11 @@ app.controller('TagsCtrl', ['$scope','$modal','$log','$http','$window','$filter'
             var subscribers = profileData.yt$statistics.subscriberCount;
 
             var views = profileData.yt$statistics.totalUploadViews;
+
+            var contentLength = profileData.content.$t.length > 250 ? true : false;
+            
+
+            // console.log(contentLength);
 
 
             $http.get("http://gdata.youtube.com/feeds/api/users/"+ influencer +"/uploads?alt=json&max-results=10")
@@ -151,7 +159,7 @@ app.controller('TagsCtrl', ['$scope','$modal','$log','$http','$window','$filter'
                 
                 var averageViews = d3.mean(averageViewsArray.map(Number));
 
-                $scope.influencersData[influencer] = { profile: profileData, videos: videoData.feed.entry, username: influencer, code: code, subscribers: subscribers, views: views, averageViews: averageViews};
+                $scope.influencersData[influencer] = { profile: profileData, videos: videoData.feed.entry, username: influencer, code: code, subscribers: subscribers, views: views, averageViews: averageViews, tooLongDescription: contentLength};
 
               });
 
